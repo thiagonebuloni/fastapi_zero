3aa86e026f924aad9caa77197b67524405ea5c31
Thiago Nebuloni <thiago.nebuloni@gmail.com>
2025-06-10 10:17:17 -0300

    Corrigido erro de pargins URL, graças ao taconi e Du

--- fastapi_zero/database.py
+++ fastapi_zero/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-engine = create_async_engine('sqlite+aiosqlite:///database.db')
+from fastapi_zero.settings import Settings
+
+engine = create_async_engine(Settings().DATABASE_URL)
 
 
 async def get_session():
